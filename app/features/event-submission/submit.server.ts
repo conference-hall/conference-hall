b36d795ce164cdd622f6bcf5f2a0dac0a1787fa3
Benjamin Petetot <bpetetot@gmail.com>
2021-12-07 23:30:01 +0100

    Submit a proposal

---
+++ app/features/event-submission/submit.server.ts
@@ -0,0 +1,85 @@
+import { ActionFunction, LoaderFunction, redirect } from 'remix';
+import { db } from '../../services/db';
+import { getCfpState } from '../../utils/event';
+import { requireUserSession } from '../auth/auth.server';
+
+export type SubmitForm = {
+  title: string;
+  speakers: Array<{ name: string; photoURL: string | null }>;
+  formats: string[];
+  categories: string[];
+};
+
+export const loadProposal: LoaderFunction = async ({ request, params }) => {
+  const uid = await requireUserSession(request);
+  const { talkId, eventSlug } = params;
+  if (!talkId) throw new Response('Talk id is required', { status: 400 });
+
+  const event = await db.event.findUnique({
+    select: { id: true, formats: true, categories: true },
+    where: { slug: eventSlug },
+  });
+  if (!event) throw new Response('Event not found', { status: 404 });
+
+  const proposal = await db.proposal.findUnique({
+    select: { title: true, speakers: true, formats: true, categories: true },
+    where: { talkId_eventId: { talkId, eventId: event.id } },
+  });
+  if (!proposal) throw new Response('Proposal not found', { status: 404 });
+
+  if (!proposal.speakers.some((s) => s.id === uid)) {
+    throw new Response('Not your proposal!', { status: 403 });
+  }
+
+  return {
+    title: proposal.title,
+    speakers: proposal.speakers.map((s) => ({ name: s.name, photoURL: s.photoURL })),
+    formats: proposal.formats.map((f) => f.name),
+    categories: proposal.categories.map((c) => c.name),
+  };
+};
+
+export const submitProposal: ActionFunction = async ({ request, params }) => {
+  const uid = await requireUserSession(request);
+  const { talkId, eventSlug } = params;
+  if (!talkId) throw new Response('Talk id is required', { status: 400 });
+
+  const event = await db.event.findUnique({
+    select: { id: true, type: true, cfpStart: true, cfpEnd: true, maxProposals: true },
+    where: { slug: eventSlug },
+  });
+  if (!event) throw new Response('Event not found', { status: 404 });
+
+  const isCfpOpen = getCfpState(event.type, event.cfpStart, event.cfpEnd) === 'OPENED';
+  if (!isCfpOpen) throw new Response('CFP is not opened', { status: 403 });
+
+  if (event.maxProposals) {
+    const nbProposals = await db.proposal.count({
+      where: {
+        eventId: event.id,
+        speakers: { some: { id: uid } },
+        status: { not: { equals: 'DRAFT' } },
+        id: { not: { equals: talkId } },
+      },
+    });
+    if (nbProposals >= event.maxProposals) {
+      throw new Response('You have reached the maximum number of proposals', { status: 403 });
+    }
+  }
+
+  const proposal = await db.proposal.findFirst({
+    where: { talkId, eventId: event.id, speakers: { some: { id: uid } } },
+  });
+  if (!proposal) throw new Response('Not your proposal!', { status: 403 });
+
+  await db.proposal.update({
+    data: { status: 'SUBMITTED' },
+    where: { talkId_eventId: { talkId, eventId: event.id } },
+  });
+
+  // TODO Email notification to speakers
+  // TODO Email notification to organizers
+  // TODO Slack notification
+
+  return redirect(`/${eventSlug}/submission`);
+};
--- app/routes/$eventSlug/submission/$talkId.submit.tsx
+++ app/routes/$eventSlug/submission/$talkId.submit.tsx
@@ -1,12 +1,18 @@
-import { Form } from 'remix';
+import { Form, useLoaderData } from 'remix';
 import { Button, ButtonLink } from '~/components/Buttons';
 import { Checkbox } from '../../../components/forms/Checkboxes';
 import { Heading } from '../../../components/Heading';
 import { usePreviousStep } from '../../../features/event-submission/hooks/usePreviousStep';
+import { loadProposal, SubmitForm, submitProposal } from '../../../features/event-submission/submit.server';
 
 export const handle = { step: 'submission' };
 
+export const loader = loadProposal
+
+export const action = submitProposal
+
 export default function EventSubmitTalkRoute() {
+  const proposal = useLoaderData<SubmitForm>()
   const previousStepPath = usePreviousStep()
 
   return (
@@ -16,15 +22,17 @@ export default function EventSubmitTalkRoute() {
           Almost done!
         </Heading>
         <p className="mt-4">
-          You are going to submit "Name of the proposal"
+          You are going to submit "{proposal.title}"
         </p>
         <p className="mt-4">
-          With speakers
+          Speakers: {proposal.speakers.map(s => s.name).join(', ')}
         </p>
         <p className="mt-4">
-          With categories and formats...
+          Formats: {proposal.formats.join(', ')}
         </p>
-       
+        <p className="mt-4">
+          Categories: {proposal.categories.join(', ')}
+        </p>
       </div>
       <div className="px-4 py-3 bg-gray-50 flex items-center justify-between sm:px-6">
         <ButtonLink to={previousStepPath} variant="secondary">
