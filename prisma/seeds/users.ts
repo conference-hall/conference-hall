b6627c410db40f1a075c7cc3d29ab674f562cb00
Benjamin Petetot <bpetetot@gmail.com>
2021-12-14 23:35:05 +0100

    Remove some default users from database

--- prisma/seeds/users.ts
+++ prisma/seeds/users.ts
@@ -1,9 +1,10 @@
 import { buildUser } from '../../tests/factories/users';
 
 export default async () => {
-  await buildUser({ id: 'tpSmd3FehZIM3Wp4HYSBnfnQmXLb', name: 'Peter Paker', email: 'spiderman@example.com' });
-  await buildUser({ id: 'xP1e9XSNLK0pJSlsQ4pC63L2U3Jt', name: 'Harley Quinn', email: 'harley.quinn@example.com' });
-  await buildUser({ id: '9licQdPND0UtBhShJ7vveJ703sJs', name: 'Clark Kent', email: 'superman@example.com' });
-  await buildUser({ id: 'xXklEkfEV3rQ01kHRh5pLO7IkX1v', name: 'Matthew Murdock', email: 'daredevil@example.com' });
-  await buildUser({ id: 'e9HDr773xNpXbOy2H0C7FDhGD2fc', name: 'Bruce Wayne', email: 'batman@example.com' });
+  await buildUser({
+    id: 'tpSmd3FehZIM3Wp4HYSBnfnQmXLb',
+    name: 'Peter Paker',
+    email: 'spiderman@example.com',
+    photoURL: 'https://www.mdcu-comics.fr/uploads/news/2020/09/news_illustre_1600620975_30.jpg',
+  });
 };
