4d176cd991f65b1f7ec30fa940f2cff3048c38a3
Benjamin Petetot <bpetetot@gmail.com>
2022-07-24 12:42:19 +0200

    Improve prisma db command for test

--- tests/db-helpers.ts
+++ tests/db-helpers.ts
@@ -1,26 +1,26 @@
-import { config } from '../app/services/config'
-import { db } from '../app/services/db'
+import { config } from '../app/services/config';
+import { db } from '../app/services/db';
 
 export async function disconnectDB() {
-  await db.$disconnect()
-  return 'db disconnected'
+  await db.$disconnect();
+  return 'db disconnected';
 }
 
 export async function resetDB() {
   if (config.isTest) {
-    await db.betaKey.deleteMany()
-    await db.invite.deleteMany()
-    await db.survey.deleteMany()
-    await db.message.deleteMany()
-    await db.rating.deleteMany()
-    await db.proposal.deleteMany()
-    await db.talk.deleteMany()
-    await db.eventFormat.deleteMany()
-    await db.eventCategory.deleteMany()
-    await db.event.deleteMany()
-    await db.organizationMember.deleteMany()
-    await db.organization.deleteMany()
-    await db.user.deleteMany()
+    await db.betaKey.deleteMany();
+    await db.invite.deleteMany();
+    await db.survey.deleteMany();
+    await db.message.deleteMany();
+    await db.rating.deleteMany();
+    await db.proposal.deleteMany();
+    await db.talk.deleteMany();
+    await db.eventFormat.deleteMany();
+    await db.eventCategory.deleteMany();
+    await db.event.deleteMany();
+    await db.organizationMember.deleteMany();
+    await db.organization.deleteMany();
+    await db.user.deleteMany();
   }
-  return 'db reset'
+  return 'db reset';
 }
