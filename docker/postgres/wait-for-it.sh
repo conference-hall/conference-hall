3c917353a42ac2db37d80d93e92fd679ff760ebc
Benjamin Petetot <bpetetot@gmail.com>
2022-07-30 14:39:51 +0200

    test

--- docker/postgres/wait-for-it.sh
+++ docker/postgres/wait-for-it.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-timeout 90s bash -c "until docker exec ch_postgres pg_isready ; do sleep 5 ; done"
+timeout 90s bash -c "until docker exec ch_postgres pg_isready -d 'postgresql://postgres@localhost:5432/conference-hall-test'; do sleep 5 ; done"
