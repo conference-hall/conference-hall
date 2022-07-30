2e68dff87a875d06edc3c51b2a0e599d6a3ff315
Benjamin Petetot <bpetetot@gmail.com>
2022-07-30 14:31:59 +0200

    change script

--- docker/postgres/wait-for-it.sh
+++ docker/postgres/wait-for-it.sh
@@ -1,22 +1,2 @@
 #!/bin/bash
-set -e 
-
-if [ -z "$1" -o -z "$2" ]
-then
-    echo "tcp-port-wait - block until specified TCP port becomes available"
-    echo "Usage: ntcp-port-wait HOST PORT"
-    exit 1
-fi
-echo Waiting for port $1:$2 to become available...
-while ! nc -z $1 $2 2>/dev/null
-do
-    let elapsed=elapsed+1
-    if [ "$elapsed" -gt 90 ] 
-    then
-        echo "TIMED OUT !"
-        exit 1
-    fi  
-    sleep 1;
-done
-
-echo "READY !"
+timeout 90s bash -c "until docker exec ch_postgres pg_isready ; do sleep 5 ; done"
