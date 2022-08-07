5274b9cd8b27ceba417a7bad593436348d157451
Benjamin Petetot <bpetetot@gmail.com>
2022-08-07 14:22:30 +0200

    Fix loading on login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -4,6 +4,8 @@ import { useSubmit } from '@remix-run/react';
 import { Button } from '../design-system/Buttons';
 import { useFirebaseSignIn } from '../services/auth/useFirebaseSignIn';
 import { isSessionValid, sessionLogin } from '../services/auth/auth.server';
+import { Container } from '../design-system/Container';
+import { H1 } from '../design-system/Typography';
 
 export const action: ActionFunction = async ({ request }) => {
   return sessionLogin(request);
@@ -19,22 +21,24 @@ export default function Login() {
   const submit = useSubmit();
   const [isSigning, signin] = useFirebaseSignIn(submit);
 
-  if (isSigning) return null;
-
   return (
-    <div className="mx-auto flex h-[calc(100vh-64px)] max-w-3xl flex-col items-center justify-center pb-64">
-      <h1 className="text-center text-6xl font-black">Log in to Conference Hall</h1>
-      <div className="mt-12 max-w-sm space-y-4 p-8">
-        <Button type="button" block onClick={signin}>
-          Continue with Google
-        </Button>
-        <Button type="button" block onClick={signin}>
-          Continue with GitHub
-        </Button>
-        <Button type="button" block onClick={signin}>
-          Continue with Twitter
-        </Button>
-      </div>
-    </div>
+    <Container className="flex flex-col items-center justify-center py-16 sm:py-64">
+      <H1 className="text-center text-4xl font-black">Log in to Conference Hall</H1>
+      {isSigning ? (
+        <p className="mt-12 space-y-4 p-8">Singing...</p>
+      ) : (
+        <div className="mt-12 max-w-sm space-y-4 p-8">
+          <Button type="button" block onClick={signin}>
+            Continue with Google
+          </Button>
+          <Button type="button" block onClick={signin}>
+            Continue with GitHub
+          </Button>
+          <Button type="button" block onClick={signin}>
+            Continue with Twitter
+          </Button>
+        </div>
+      )}
+    </Container>
   );
 }
