9b041704366e2b71a8fe9448028b2e3a6d8ee0b0
Benjamin Petetot <bpetetot@gmail.com>
2022-09-03 22:40:02 +0200

    Prepare some organizer redirection rules

--- app/routes/organizer/slug.tsx
+++ app/routes/organizer/slug.tsx
@@ -1,26 +1,17 @@
 import type { LoaderFunction } from '@remix-run/node';
-import { redirect } from '@remix-run/node';
 import { Container } from '~/design-system/Container';
 import { sessionRequired } from '~/services/auth/auth.server';
 import { H1 } from '~/design-system/Typography';
 
 export const loader: LoaderFunction = async ({ request }) => {
   await sessionRequired(request);
-
-  const hasOrganizerAccess = true;
-  if (!hasOrganizerAccess) return redirect('/organizer/request');
-
-  const organizations = 0;
-  if (organizations === 0) return redirect('/organizer/welcome');
-  if (organizations === 1) return redirect('/organizer/slug');
-
-  return organizations;
+  return null;
 };
 
 export default function OrganizerRoute() {
   return (
     <Container className="my-4 sm:my-8">
-      <H1>My organizations</H1>
+      <H1>Clark kent's orga</H1>
     </Container>
   );
 }
