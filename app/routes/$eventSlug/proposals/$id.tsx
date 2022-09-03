0a4f115699c65582af0b99d7ff2ca6ceaf0984c5
Benjamin Petetot <bpetetot@gmail.com>
2022-09-03 12:52:32 +0200

    Make event proposal page responsive

--- app/routes/$eventSlug/proposals/$id.tsx
+++ app/routes/$eventSlug/proposals/$id.tsx
@@ -5,7 +5,7 @@ import { useEvent } from '../../$eventSlug';
 import { ButtonLink } from '../../../design-system/Buttons';
 import { IconLabel } from '../../../design-system/IconLabel';
 import { Markdown } from '../../../design-system/Markdown';
-import { H1, H2 } from '../../../design-system/Typography';
+import { H2, H3 } from '../../../design-system/Typography';
 import type { ActionFunction, LoaderArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
 import { sessionRequired } from '../../../services/auth/auth.server';
@@ -45,10 +45,10 @@ export default function EventSpeakerProposalRoute() {
   const proposal = useLoaderData<typeof loader>();
 
   return (
-    <Container className="mt-8">
-      <div className="flex flex-wrap items-center justify-between sm:flex-nowrap">
+    <Container className="my-4 sm:my-8">
+      <div className="flex flex-col flex-wrap sm:flex-row sm:items-center sm:justify-between">
         <div>
-          <H1>{proposal.title}</H1>
+          <H2>{proposal.title}</H2>
           <div className="mt-2 flex gap-2">
             <Badge color="indigo">{getLevel(proposal.level)}</Badge>
             {proposal.languages.map((language) => (
@@ -60,7 +60,7 @@ export default function EventSpeakerProposalRoute() {
         </div>
 
         {event.cfpState === 'OPENED' && (
-          <div className="flex-shrink-0 space-x-4">
+          <div className="mt-4 flex flex-col justify-between gap-4 sm:mt-0 sm:flex-shrink-0 sm:flex-row">
             <EventProposalDeleteButton />
             {proposal.status === 'DRAFT' ? (
               <ButtonLink to={`../submission/${proposal.talkId}`}>Submit proposal</ButtonLink>
@@ -72,34 +72,36 @@ export default function EventSpeakerProposalRoute() {
       </div>
 
       {proposal.status === 'DRAFT' && (
-        <div className="mt-8">
-          <IconLabel icon={ExclamationCircleIcon} className="text-sm text-yellow-600">
-            This proposal is still in draft. Don't forget to submit it.
-          </IconLabel>
-        </div>
+        <IconLabel icon={ExclamationCircleIcon} className="mt-8 text-sm text-yellow-600">
+          This proposal is still in draft. Don't forget to submit it.
+        </IconLabel>
       )}
 
-      <div className="mt-8 flex flex-row gap-4">
-        <div className="w-2/3 overflow-hidden border border-gray-200 bg-white p-4 sm:rounded-lg">
-          <H2>Abstract</H2>
+      <div className="mt-8 flex flex-col gap-4 sm:flex-row">
+        <div className="rounded-lg border border-gray-200 p-4 sm:w-2/3">
+          <H3>Abstract</H3>
           <Markdown source={proposal.abstract} className="mt-2" />
-          <H2 className="mt-8">References</H2>
-          <Markdown source={proposal.references} className="mt-2" />
+          {proposal.references && (
+            <>
+              <H3 className="mt-8">References</H3>
+              <Markdown source={proposal.references} className="mt-2" />
+            </>
+          )}
         </div>
-        <div className="w-1/3">
-          <div className="overflow-hidden border border-gray-200 bg-white p-4 sm:rounded-lg">
-            <H2>Speakers</H2>
+        <div className="sm:w-1/3">
+          <div className="rounded-lg border border-gray-200 p-4">
+            <H3>Speakers</H3>
             <CoSpeakersList speakers={proposal.speakers} showRemoveAction={event.cfpState === 'OPENED'} />
             {event.cfpState === 'OPENED' && (
               <InviteCoSpeakerButton to="PROPOSAL" id={proposal.id} invitationLink={proposal.invitationLink} />
             )}
           </div>
-          <dl className="mt-4 overflow-hidden border border-gray-200 bg-white p-4 sm:rounded-lg">
-            <H2 as="dt">Formats</H2>
+          <dl className="mt-4 rounded-lg border border-gray-200 p-4">
+            <H3 as="dt">Formats</H3>
             <dd className="mt-4 text-sm text-gray-900">{proposal.formats.map(({ name }) => name).join(', ') || '—'}</dd>
-            <H2 as="dt" className="mt-8">
+            <H3 as="dt" className="mt-8">
               Categories
-            </H2>
+            </H3>
             <dd className="mt-4 text-sm text-gray-900">
               {proposal.categories.map(({ name }) => name).join(', ') || '—'}
             </dd>
