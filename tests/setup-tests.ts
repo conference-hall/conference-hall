fa9d13c42858e6ee7696c30f4ec6904c52249745
Benjamin Petetot <bpetetot@gmail.com>
2022-11-11 17:55:42 +0100

    Set fix for vitest@0.25.1

--- tests/setup-tests.ts
+++ tests/setup-tests.ts
@@ -1,5 +1,11 @@
+import { URL } from 'node:url';
 import { vi } from 'vitest';
 import { installGlobals } from '@remix-run/node';
+
+// Fix: happy-dom has a bug when parsing URL
+// See https://github.com/capricorn86/happy-dom/issues/569
+// @ts-ignore
+globalThis.URL = URL;
 
 // This installs globals such as "fetch", "Response", "Request" and "Headers.
 installGlobals();
