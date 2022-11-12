884bd20908952c5809718c36c38e442f08ea5f87
Benjamin Petetot <bpetetot@gmail.com>
2022-11-12 21:30:52 +0100

    Add attributes to export

--- app/services/organizers/event.spec.ts
+++ app/services/organizers/event.spec.ts
@@ -3,6 +3,7 @@ import { MessageChannel } from '@prisma/client';
 import {
   addProposalComment,
   checkOrganizerEventAccess,
+  exportProposalsFromFilters,
   removeProposalComment,
   updateProposalsStatus,
 } from './event.server';
@@ -1105,5 +1106,60 @@ describe('#updateProposalsStatus', () => {
     await expect(
       updateProposalsStatus(organization.slug, event.slug, user.id, [proposal.id], 'ACCEPTED')
     ).rejects.toThrowError(ForbiddenOperationError);
+  });
+});
+
+describe.only('#exportProposalsFromFilters', () => {
+  let owner: User, reviewer: User, speaker: User;
+  let organization: Organization;
+  let event: Event;
+
+  beforeEach(async () => {
+    await resetDB();
+    owner = await userFactory();
+    reviewer = await userFactory();
+    speaker = await userFactory();
+    organization = await organizationFactory({ owners: [owner], reviewers: [reviewer] });
+    event = await eventFactory({ organization });
+  });
+  afterEach(disconnectDB);
+
+  it('export a proposal', async () => {
+    const proposal = await proposalFactory({ event, talk: await talkFactory({ speakers: [speaker] }) });
+
+    const result = await exportProposalsFromFilters(organization.slug, event.slug, owner.id, {});
+
+    expect(result).toEqual([
+      {
+        id: proposal.id,
+        title: proposal.title,
+        status: proposal.status,
+        abstract: proposal.abstract,
+        comments: proposal.comments,
+        languages: proposal.languages,
+        references: proposal.references,
+        level: proposal.level,
+        categories: [],
+        formats: [],
+        ratings: {
+          negatives: 0,
+          positives: 0,
+          total: null,
+        },
+        speakers: [
+          {
+            name: speaker.name,
+            email: speaker.email,
+            bio: speaker.bio,
+            photoURL: speaker.photoURL,
+            company: speaker.company,
+            address: speaker.address,
+            references: speaker.references,
+            github: speaker.github,
+            twitter: speaker.twitter,
+          },
+        ],
+      },
+    ]);
   });
 });
