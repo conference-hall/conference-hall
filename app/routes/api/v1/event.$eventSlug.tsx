a5ad3d2f2ac5e03b0066c87f75db064992102778
Benjamin Petetot <bpetetot@gmail.com>
2022-11-13 13:31:35 +0100

    Add proposal API

---
+++ app/routes/api/v1/event.$eventSlug.tsx
@@ -0,0 +1,25 @@
+import type { LoaderArgs } from '@remix-run/node';
+import { json } from '@remix-run/node';
+import { errorMessagesForSchema, inputFromUrl } from 'domain-functions';
+import { EventProposalsApiSchema, getEventProposals } from '~/services/api/get-event-proposals.server';
+import { fromErrors } from '~/services/errors';
+
+export const loader = async ({ request, params }: LoaderArgs) => {
+  const result = await getEventProposals({
+    ...params,
+    ...inputFromUrl(request),
+  });
+
+  if (result.success) {
+    return json(result.data);
+  }
+
+  if (result.errors.length > 0) {
+    throw fromErrors(result);
+  }
+
+  if (result.inputErrors.length > 0) {
+    const errors = errorMessagesForSchema(result.inputErrors, EventProposalsApiSchema);
+    throw new Response(JSON.stringify(errors), { status: 400 });
+  }
+};
---
+++ app/services/api/get-event-proposals.spec.ts
@@ -0,0 +1,43 @@
+import { disconnectDB, resetDB } from 'tests/db-helpers';
+import { eventFactory } from 'tests/factories/events';
+import { proposalFactory } from 'tests/factories/proposals';
+import { talkFactory } from 'tests/factories/talks';
+import { userFactory } from 'tests/factories/users';
+import { getEventProposals } from './get-event-proposals.server';
+
+describe('#getEventProposals', () => {
+  beforeEach(async () => {
+    await resetDB();
+  });
+  afterEach(disconnectDB);
+
+  it('return proposals from api', async () => {
+    const speaker = await userFactory();
+    const event = await eventFactory({ attributes: { apiKey: '123' } });
+    const proposal = await proposalFactory({ event, talk: await talkFactory({ speakers: [speaker] }) });
+
+    const result = await getEventProposals({ eventSlug: event.slug, apiKey: event.apiKey });
+
+    expect(result.success && result.data).toEqual([
+      {
+        id: proposal.id,
+        title: proposal.title,
+        abstract: proposal.abstract,
+        languages: proposal.languages,
+        level: proposal.level,
+        categories: [],
+        formats: [],
+        speakers: [
+          {
+            name: speaker.name,
+            bio: speaker.bio,
+            photoURL: speaker.photoURL,
+            company: speaker.company,
+            github: speaker.github,
+            twitter: speaker.twitter,
+          },
+        ],
+      },
+    ]);
+  });
+});
