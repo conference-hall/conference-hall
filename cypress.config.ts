e4565a73facc37de1902386a3c36c29891273aa5
Benjamin Petetot <bpetetot@gmail.com>
2023-03-02 22:41:56 +0100

    fix

--- cypress.config.ts
+++ cypress.config.ts
@@ -15,8 +15,17 @@ export default defineConfig({
     baseUrl: config.appUrl,
     scrollBehavior: 'center',
     setupNodeEvents(on) {
+      on('before:browser:launch', (browser, launchOptions) => {
+        console.log(launchOptions.args);
+
+        if (browser.name == 'chrome') {
+          launchOptions.args.push('--disable-gpu');
+        }
+
+        return launchOptions;
+      });
       // setup custom task
-      return on('task', {
+      on('task', {
         disconnectDB,
         seedDB: async (name: string) => {
           try {
