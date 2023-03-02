4b9f0f66fa1b6b4a81f6f3de73eee4436d43240b
Benjamin Petetot <bpetetot@gmail.com>
2023-03-03 00:15:57 +0100

    fix

---
+++ app/entry.client.tsx
@@ -0,0 +1,9 @@
+import * as ReactDOMClient from 'react-dom/client';
+import { RemixBrowser } from '@remix-run/react';
+
+if (process.env.NODE_ENV === 'production') {
+  ReactDOMClient.hydrateRoot(document, <RemixBrowser />);
+} else {
+  // Bug in Cypress with hydration of the DOM: https://github.com/remix-run/remix/issues/2570
+  require('react-dom').hydrate(<RemixBrowser />, document);
+}
