d289fc43fb4a439a056ce19e8508d28326e1eead
Benjamin Petetot <bpetetot@gmail.com>
2023-03-04 12:25:23 +0100

    Fix

--- app/libs/db.ts
+++ app/libs/db.ts
@@ -12,7 +12,7 @@ declare global {
 // this is needed because in development we don't want to restart
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
-if (config.isProduction) {
+if (config.isProduction && !config.useEmulators) {
   db = getClient();
 } else {
   if (!global.__db) {
