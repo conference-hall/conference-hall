5dfa652591780d4c8cdd8a1e5987edffaf32ba04
Benjamin Petetot <bpetetot@gmail.com>
2023-04-22 22:27:21 +0200

    Manage service account env variable

--- app/libs/auth/firebase.server.ts
+++ app/libs/auth/firebase.server.ts
@@ -1,3 +1,4 @@
+import { credential } from 'firebase-admin';
 import type { App } from 'firebase-admin/app';
 import { initializeApp, getApps, getApp } from 'firebase-admin/app';
 import type { Auth } from 'firebase-admin/auth';
@@ -8,10 +9,18 @@ let app: App;
 let auth: Auth;
 
 if (getApps().length === 0) {
-  app = initializeApp({
-    projectId: config.FIREBASE_PROJECT_ID,
-    storageBucket: config.FIREBASE_STORAGE,
-  });
+  if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    app = initializeApp({
+      projectId: config.FIREBASE_PROJECT_ID,
+      storageBucket: config.FIREBASE_STORAGE,
+      credential: credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)),
+    });
+  } else {
+    app = initializeApp({
+      projectId: config.FIREBASE_PROJECT_ID,
+      storageBucket: config.FIREBASE_STORAGE,
+    });
+  }
   auth = getAuth(app);
 } else {
   app = getApp();
