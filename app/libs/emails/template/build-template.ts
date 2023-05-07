dee1607064e4b16fb1d0469e71451c9def338b98
Benjamin Petetot <bpetetot@gmail.com>
2023-05-07 17:50:22 +0200

    Upgrade dependencies

--- app/libs/emails/template/build-template.ts
+++ app/libs/emails/template/build-template.ts
@@ -2,6 +2,8 @@ import fs from 'node:fs';
 import xss from 'xss';
 import { marked } from 'marked';
 
+marked.use({ mangle: false, headerIds: false });
+
 export function buildTemplate(subject: string, template: string, variables: Record<string, string> = {}) {
   const base = fs.readFileSync(`./app/libs/emails/template/email-base.html`);
 
