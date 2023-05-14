8bbdc6aeb03ba57bb8ab1ec25a008d08aadca1e5
Benjamin Petetot <bpetetot@gmail.com>
2023-05-14 12:27:23 +0200

    Rework pagination component

--- app/design-system/Pagination.tsx
+++ app/design-system/Pagination.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid';
 import { Link, useLocation, useSearchParams } from '@remix-run/react';
 import c from 'classnames';
 import { useMemo } from 'react';
@@ -20,14 +21,30 @@ export function Pagination({ current, total, className }: Props) {
 
   const pages = useMemo(() => Array.from({ length: total }, (_, i) => i + 1), [total]);
 
+  const previous = current - 1 > 0 ? current - 1 : 1;
+  const next = current + 1 <= total ? current + 1 : current;
+
   if (total <= 1) return null;
 
   return (
-    <nav className={c('flex items-center justify-center border-t border-gray-200 px-4 sm:px-0', className)}>
+    <nav
+      className={c('isolate inline-flex -space-x-px rounded-md bg-white shadow-sm', className)}
+      aria-label="Pagination"
+    >
+      <Link
+        key="previous"
+        to={{ pathname: location.pathname, search: getPageSearchParams(previous, searchParams) }}
+        preventScrollReset
+        className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+      >
+        <span className="sr-only">Previous</span>
+        <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+      </Link>
       {pages.map((page) => {
-        const styles = c('border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium', {
-          'border-indigo-500 text-indigo-600': page === current,
-          'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300': page !== current,
+        const styles = c('relative inline-flex items-center px-4 py-2 text-sm font-semibold focus:z-2', {
+          'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600':
+            page === current,
+          'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0': page !== current,
         });
 
         if (showPageButton(page, current, total)) {
@@ -44,13 +61,22 @@ export function Pagination({ current, total, className }: Props) {
           );
         } else if (showPageButton(page - 1, current, total)) {
           return (
-            <div key="separator" className="inline-flex items-center px-4 pt-4 text-sm font-medium text-gray-500">
+            <div key="separator" className={styles}>
               ...
             </div>
           );
         }
         return null;
       })}
+      <Link
+        key="previous"
+        to={{ pathname: location.pathname, search: getPageSearchParams(next, searchParams) }}
+        preventScrollReset
+        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+      >
+        <span className="sr-only">Next</span>
+        <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+      </Link>
     </nav>
   );
 }
