b4ab531892b128106158ce5a28a46756bf16a2d2
Benjamin Petetot <bpetetot@gmail.com>
2023-08-06 18:10:58 +0200

    Simplify vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,21 +1,18 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 const isCI = Boolean(process.env.CI);
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
     threads: false,
-    environment: 'happy-dom',
     setupFiles: ['./tests/setup-tests.ts'],
-    exclude: ['./cypress', './node_modules'],
-    watchExclude: ['.*\\/node_modules\\/.*', '.*\\/build\\/.*'],
+    include: ['./app/**/*.{test,spec}.ts?(x)'],
     reporters: isCI ? ['default', 'junit'] : 'default',
     outputFile: './test-results/unit.xml',
   },
