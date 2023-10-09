9450f13fd719ae3d035740434fbe91ca47f92e13
Benjamin Petetot <516360+bpetetot@users.noreply.github.com>
2023-10-09 23:39:45 +0200

    Add Content-Security-Policy (#136)

---
+++ app/utils/useNonce.ts
@@ -0,0 +1,30 @@
+import { createContext, useContext } from 'react';
+
+/**
+ * Context containing a server-side nonce for CSP purposes.
+ *
+ * **_Only provide a value on the server (in entry.server.tsx)! We don't want this ending up in
+ * client bundles._**
+ *
+ * On the client this should just return an empty string.
+ */
+export const NonceContext = createContext('');
+
+/**
+ * Get a "nonce", which is a random string generated for each HTTP request.
+ *
+ * This app's CSP directives are configured to recognize it. Pass to any `<script>` tags so that
+ * browsers know they are trused.
+ *
+ * @see https://content-security-policy.com/nonce/
+ *
+ * @example
+ * const nonce = useNonce();
+ *
+ * <div>
+ *   <script src="./some-src.js" nonce={nonce} />
+ * </div>
+ */
+export function useNonce() {
+  return useContext(NonceContext);
+}
