50d51676285d044b50d6916719e215dc9519d02b
Benjamin Petetot <bpetetot@gmail.com>
2023-10-09 17:39:52 +0200

    Simplify config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,7 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    '@remix-run/eslint-config',
-    '@remix-run/eslint-config/node',
-    '@remix-run/eslint-config/jest-testing-library',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', '@remix-run/eslint-config', '@remix-run/eslint-config/node', 'prettier'],
   plugins: ['simple-import-sort', 'prettier'],
-  // we're using vitest which has a very similar API to jest
-  // (so the linting plugins work nicely), but it we have to explicitly
-  // set the jest version.
-  settings: {
-    jest: {
-      version: 28,
-    },
-  },
   rules: {
     'prettier/prettier': 'warn',
     'jsx-a11y/anchor-has-content': 'off',
