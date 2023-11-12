49aad351718917f571df644888696fdf853339b4
Benjamin Petetot <bpetetot@gmail.com>
2023-11-12 18:10:16 +0100

    Display campaign template preview

--- app/domain/email-campaigns/campaign-templates.server.ts
+++ app/domain/email-campaigns/campaign-templates.server.ts
@@ -3,6 +3,7 @@ type CampaignTemplate = Array<{
   title: string;
   description: string;
   segments: Array<{ status: string }>;
+  email_from: string;
   email_subject: string;
   email_template: string;
 }>;
@@ -13,18 +14,19 @@ export const campaignTemplates: CampaignTemplate = [
     title: 'Accepted proposals emails',
     description: 'Send an email to speakers to confirm or declined their presence to the conference.',
     segments: [{ status: 'ACCEPTED' }],
+    email_from: `[%eventName%] <no-reply@conference-hall.io>`,
     email_subject: `[%eventName%] Your talk has been accepted`,
     email_template: `Hi %recipient.fullname%,
 
-    Your talk **%recipient.proposalTitle%** at **%eventName%** has been accepted.
-    
-    In order to help organizers for the selection and the event management, you can confirm or decline your participation to %eventName%.
-    
-    [**Please confirm or decline.**](%appUrl%/%eventSlug%/proposals/%recipient.proposalId%)
-    
-    See you there!
-    
-    %eventName% team.
+Your talk **%recipient.proposalTitle%** at **%eventName%** has been accepted.
+
+In order to help organizers for the selection and the event management, you can confirm or decline your participation to %eventName%.
+
+[**Please confirm or decline.**](%appUrl%/%eventSlug%/proposals/%recipient.proposalId%)
+
+See you there!
+
+%eventName% team.
     `,
   },
   {
@@ -32,16 +34,17 @@ export const campaignTemplates: CampaignTemplate = [
     title: 'Rejected proposals emails',
     description: 'Send an email to speakers for rejected proposals.',
     segments: [{ status: 'REJECTED' }],
+    email_from: `[%eventName%] <no-reply@conference-hall.io>`,
     email_subject: '[%eventName%] Your talk has been declined',
     email_template: `Dear %recipient.fullname%,
-    
-    Your talk **%recipient.proposalTitle%** at **%eventName%** has been declined.
-    
-    We had lots of excellent talks this year and choosing among them has been heart-breaking. 😓 
-    
-    Thank you very much for your submission and please consider submitting again next year.
-    
-    %eventName% team.
+
+Your talk **%recipient.proposalTitle%** at **%eventName%** has been declined.
+
+We had lots of excellent talks this year and choosing among them has been heart-breaking. 😓 
+
+Thank you very much for your submission and please consider submitting again next year.
+
+%eventName% team.
     `,
   },
 ];
--- app/routes/team_.$team.$event.emails.new+/$template.index.tsx
+++ app/routes/team_.$team.$event.emails.new+/$template.index.tsx
@@ -1,24 +1,55 @@
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import type { LoaderFunctionArgs } from '@remix-run/node';
 import { json } from '@remix-run/node';
+import { useLoaderData } from '@remix-run/react';
 import invariant from 'tiny-invariant';
 
+import { Badge } from '~/design-system/Badges.tsx';
 import { ButtonLink } from '~/design-system/Buttons.tsx';
+import { Card } from '~/design-system/layouts/Card.tsx';
+import { Markdown } from '~/design-system/Markdown.tsx';
+import { H2, Subtitle, Text } from '~/design-system/Typography.tsx';
+import { campaignTemplates } from '~/domain/email-campaigns/campaign-templates.server.ts';
 import { requireSession } from '~/libs/auth/session.ts';
 
 export const loader = async ({ request, params }: LoaderFunctionArgs) => {
   await requireSession(request);
   invariant(params.event, 'Invalid event slug');
+  invariant(params.template, 'Invalid temaplte name');
 
-  return json({});
+  const template = campaignTemplates.find((template) => template.name === params.template);
+
+  return json(template);
 };
 
 export default function CampaignTemplatePreview() {
+  const template = useLoaderData<typeof loader>();
+
   return (
-    <div>
-      <ButtonLink to="selection" iconRight={ArrowRightIcon}>
-        Continue
-      </ButtonLink>
+    <div className="space-y-8">
+      <Card p={8}>
+        <H2>{template.title}</H2>
+        <Subtitle>{template.description}</Subtitle>
+      </Card>
+      <Card>
+        <div className="flex justify-between items-center px-8 py-4">
+          <Text weight="semibold">From: {template.email_from}</Text>
+          <Badge>Email preview</Badge>
+        </div>
+        <div className="border-b border-b-gray-200" />
+        <div className="px-8 py-4">
+          <Markdown>{template.email_subject}</Markdown>
+        </div>
+        <div className="border-b border-b-gray-200" />
+        <div className="p-8">
+          <Markdown>{template.email_template}</Markdown>
+        </div>
+      </Card>
+      <div className="flex justify-end">
+        <ButtonLink to="selection" iconRight={ArrowRightIcon}>
+          Go to proposals selection
+        </ButtonLink>
+      </div>
     </div>
   );
 }
