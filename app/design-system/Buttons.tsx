d2b76487a6184514555f29213a7533b555a05860
Benjamin Petetot <bpetetot@gmail.com>
2023-11-28 00:13:20 +0100

    Fix button style

--- app/design-system/Buttons.tsx
+++ app/design-system/Buttons.tsx
@@ -6,19 +6,19 @@ import React from 'react';
 
 export const button = cva(
   [
-    'inline-flex justify-center items-center w-full sm:w-auto',
-    'font-medium whitespace-nowrap border rounded-md shadow-sm',
-    'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500',
+    'inline-flex items-center gap-x-2',
+    'font-medium whitespace-nowrap rounded-md shadow-sm',
+    'focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600',
   ],
   {
     variants: {
       variant: {
-        primary: 'text-white bg-indigo-600 hover:bg-indigo-700 border-transparent',
-        secondary: 'text-gray-700 bg-white hover:bg-gray-50 border-gray-300',
+        primary: 'text-white bg-indigo-600 hover:bg-indigo-500',
+        secondary: 'text-gray-700 bg-white hover:bg-gray-50 ring-1 ring-inset ring-gray-300',
       },
       size: {
-        s: 'px-2.5 py-1.5 text-xs',
-        m: 'px-4 py-2 text-sm',
+        s: 'px-2.5 py-1.5 text-xs font-semibold',
+        m: 'px-3 py-2 text-sm font-semibold',
       },
       disabled: { true: 'opacity-50 cursor-not-allowed' },
       loading: { true: 'opacity-50 cursor-not-allowed' },
@@ -35,10 +35,10 @@ const icon = cva('', {
     dir: { left: '', right: '' },
   },
   compoundVariants: [
-    { size: 's', dir: 'left', className: '-ml-0.5 mr-2' },
-    { size: 's', dir: 'right', className: 'ml-2 -mr-0.5' },
-    { size: 'm', dir: 'left', className: '-ml-1 mr-2' },
-    { size: 'm', dir: 'right', className: 'ml-2 -mr-1' },
+    { size: 's', dir: 'left', className: '-ml-0.5' },
+    { size: 's', dir: 'right', className: '-mr-0.5' },
+    { size: 'm', dir: 'left', className: '-ml-1' },
+    { size: 'm', dir: 'right', className: '-mr-1' },
   ],
   defaultVariants: { size: 'm' },
 });
