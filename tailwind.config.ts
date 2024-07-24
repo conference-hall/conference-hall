5a748b31dac822f837fe66a1bd649dbad2e4120a
Benjamin Petetot <bpetetot@gmail.com>
2024-07-24 18:54:55 +0200

    Optimize fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        sans: ['var(--font-sans)', ...defaultTheme.fontFamily.sans],
       },
     },
   },
