a0383288402be0f92d9b3fa48fc2a888d5b1866f
Emmanuel <demey.emmanuel@gmail.com>
2024-07-25 21:27:22 +0100

    feat: add v0 for CSV export

--- app/routes/team+/$team.$event+/reviews+/__components/proposals-list-page/actions/export-menu.tsx
+++ app/routes/team+/$team.$event+/reviews+/__components/proposals-list-page/actions/export-menu.tsx
@@ -10,6 +10,7 @@ import { MenuTransition } from '~/design-system/transitions.tsx';
 import { useTeam } from '~/routes/team+/__components/use-team.tsx';
 
 const exportItems = [
+  { name: 'As CSV', path: 'csv', icon: CodeBracketIcon },
   { name: 'As JSON', path: 'json', icon: CodeBracketIcon },
   { name: 'As printable cards', path: 'cards', icon: Squares2X2Icon },
 ];
