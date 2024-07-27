1ff7de1bbd58dc5af96bdfc0c6d31e77a09f9ea7
Benjamin Petetot <bpetetot@gmail.com>
2024-07-27 16:07:24 +0200

    Fix tests

--- app/.server/speaker-activities/speaker-activities.test.ts
+++ app/.server/speaker-activities/speaker-activities.test.ts
@@ -1,6 +1,7 @@
 import { eventFactory } from 'tests/factories/events.ts';
 import { proposalFactory } from 'tests/factories/proposals.ts';
 import { talkFactory } from 'tests/factories/talks.ts';
+import { teamFactory } from 'tests/factories/team.ts';
 import { userFactory } from 'tests/factories/users.ts';
 
 import { SpeakerProposalStatus } from '~/types/speaker.types.ts';
@@ -10,8 +11,9 @@ import { SpeakerActivities } from './speaker-activities.ts';
 describe('SpeakerActivities', () => {
   describe('list', () => {
     it('returns speaker activity with proposal submitted ordered by update date', async () => {
-      const event = await eventFactory({ traits: ['conference-cfp-open'] });
-      const event2 = await eventFactory();
+      const team = await teamFactory();
+      const event = await eventFactory({ traits: ['conference-cfp-open'], team });
+      const event2 = await eventFactory({ team });
 
       const speaker = await userFactory();
       const talk = await talkFactory({ speakers: [speaker] });
@@ -25,13 +27,13 @@ describe('SpeakerActivities', () => {
           {
             slug: event2.slug,
             name: event2.name,
+            teamName: team.name,
             logoUrl: event2.logoUrl,
             cfpState: 'CLOSED',
             submissions: [
               {
                 id: proposal2.id,
                 title: proposal2.title,
-                updatedAt: proposal2.updatedAt.toISOString(),
                 status: SpeakerProposalStatus.DeliberationPending,
                 speakers: [{ id: speaker.id, name: speaker.name, picture: speaker.picture }],
               },
@@ -40,13 +42,13 @@ describe('SpeakerActivities', () => {
           {
             slug: event.slug,
             name: event.name,
+            teamName: team.name,
             logoUrl: event.logoUrl,
             cfpState: 'OPENED',
             submissions: [
               {
                 id: proposal.id,
                 title: proposal.title,
-                updatedAt: proposal.updatedAt.toISOString(),
                 status: SpeakerProposalStatus.Submitted,
                 speakers: [{ id: speaker.id, name: speaker.name, picture: speaker.picture }],
               },
