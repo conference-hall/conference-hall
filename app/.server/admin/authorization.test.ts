8ecef23b6b325446b01bb4102918c6d347401b1a
Benjamin Petetot <bpetetot@gmail.com>
2024-08-11 18:01:10 +0200

    Add tests

--- app/.server/admin/authorization.test.ts
+++ app/.server/admin/authorization.test.ts
@@ -1,7 +1,19 @@
+import { userFactory } from 'tests/factories/users.ts';
+import { NotAuthorizedError } from '~/libs/errors.server.ts';
+import { needsAdminRole } from './authorization.ts';
+
 describe('#needsAdminRole', () => {
-  it.todo('allows admin');
+  it('allows admin', async () => {
+    const user = await userFactory({ attributes: { admin: true } });
+    await expect(needsAdminRole(user.id)).resolves.not.toThrow();
+  });
 
-  it.todo('throws an error when user is not found');
+  it('throws an error when user is not found', async () => {
+    await expect(needsAdminRole('123')).rejects.toThrowError(NotAuthorizedError);
+  });
 
-  it.todo('throws an error when user is not admin');
+  it('throws an error when user is not admin', async () => {
+    const user = await userFactory();
+    await expect(needsAdminRole(user.id)).rejects.toThrowError(NotAuthorizedError);
+  });
 });
