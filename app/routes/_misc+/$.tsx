ad7ca0de296b6d555bc9918e8203dbdc08dd5743
Benjamin Petetot <516360+bpetetot@users.noreply.github.com>
2024-09-29 20:35:35 +0200

    fix: error management on session and splat route (#294)

--- app/routes/_misc+/$.tsx
+++ app/routes/_misc+/$.tsx
@@ -1,7 +1,12 @@
 import { ErrorPage } from '../__components/errors/error-page.tsx';
 import { NotFound } from '../__components/errors/not-found.tsx';
 
-// Splat route used to manage all unknown routes
+// Splat routes used to manage POST/PUT/PATCH unknown routes
+export async function action() {
+  throw new Response(null, { status: 404, statusText: 'Not Found' });
+}
+
+// Splat route used to manage GET unknown routes
 export default function NotFoundPage() {
   return (
     <ErrorPage>
