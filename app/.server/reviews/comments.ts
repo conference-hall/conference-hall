3fbe955d921b0d5ef9ed6204d022706c100ced28
Benjamin Petetot <bpetetot@gmail.com>
2024-10-27 17:33:32 +0100

    feat: add server for comment reactions

--- app/.server/reviews/comments.ts
+++ app/.server/reviews/comments.ts
@@ -1,6 +1,8 @@
 import { db } from 'prisma/db.server.ts';
 
+import type { EmojiReaction } from '~/routes/__components/emojis/emojis.ts';
 import { UserEvent } from '../event-settings/user-event.ts';
+import type { CommentReactionData } from './proposal-review.types.ts';
 
 export class Comments {
   constructor(
@@ -22,9 +24,58 @@ export class Comments {
     });
   }
 
-  async remove(messageId: string) {
+  async remove(commentId: string) {
     await this.userEvent.needsPermission('canAccessEvent');
 
-    await db.comment.deleteMany({ where: { id: messageId, userId: this.userId, proposalId: this.proposalId } });
+    await db.comment.deleteMany({ where: { id: commentId, userId: this.userId, proposalId: this.proposalId } });
+  }
+
+  // TODO: Add tests
+  async reactToComment({ commentId, code }: CommentReactionData) {
+    await this.userEvent.needsPermission('canAccessEvent');
+
+    const existingReaction = await db.commentReaction.findUnique({
+      where: { userId_commentId_code: { userId: this.userEvent.userId, commentId, code } },
+    });
+
+    // delete
+    if (existingReaction) {
+      return db.commentReaction.delete({
+        where: { userId_commentId_code: { userId: this.userEvent.userId, commentId, code } },
+      });
+    }
+
+    // create
+    return db.commentReaction.create({ data: { userId: this.userEvent.userId, commentId, code } });
+  }
+
+  // TODO: Add tests
+  static async listReactions(commentIds: Array<string>, currentUserId: string) {
+    if (commentIds.length === 0) return {};
+
+    const reactions = await db.commentReaction.findMany({
+      where: { commentId: { in: commentIds } },
+      include: { reactedBy: true },
+    });
+
+    return commentIds.reduce<Record<string, Array<EmojiReaction>>>((byComments, commentId) => {
+      const commentReactions = reactions.filter((reaction) => reaction.commentId === commentId);
+
+      byComments[commentId] = commentReactions.reduce<Array<EmojiReaction>>((byCode, reaction) => {
+        const reacted = reaction.userId === currentUserId;
+        const reactedBy = reacted ? 'You' : reaction.reactedBy.name;
+
+        const existing = byCode.find((r) => r.code === reaction.code);
+        if (!existing) {
+          byCode.push({ code: reaction.code, reacted, reactedBy: [reactedBy] });
+        } else {
+          existing.reacted = existing.reacted || reacted;
+          existing.reactedBy.push(reactedBy);
+        }
+        return byCode;
+      }, []);
+
+      return byComments;
+    }, {});
   }
 }
