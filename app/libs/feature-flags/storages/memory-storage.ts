2060f0bb7737803f6e9b380c73a7621ba03fa022
Benjamin Petetot <516360+bpetetot@users.noreply.github.com>
2024-11-05 21:59:12 +0100

    refactor: improve flags lib (#332)

--- app/libs/feature-flags/storages/memory-storage.ts
+++ app/libs/feature-flags/storages/memory-storage.ts
@@ -2,13 +2,13 @@ import type { FlagConfig } from '../types.ts';
 import type { FlagsStorage } from './flags-storage.ts';
 
 export class MemoryStorage implements FlagsStorage {
-  private storage = new Map<string, any>();
+  private storage: Map<string, any> = new Map();
 
-  async getValue(key: string, _config: FlagConfig): Promise<any> {
+  async getValue(key: string): Promise<any> {
     return this.storage.get(key);
   }
 
-  async getValues(keys: string[], _config: FlagConfig): Promise<Record<string, any>> {
+  async getValues(keys: string[]): Promise<Record<string, any>> {
     const result: Record<string, any> = {};
     for (const key of keys) {
       result[key] = this.storage.get(key);
