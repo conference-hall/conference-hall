15d6aaae8800904ac1836118ccf198231e7ed5de
Benjamin Petetot <bpetetot@gmail.com>
2024-11-15 22:31:22 +0100

    feat: add email debug page for admin

---
+++ app/.server/admin/admin-debug.ts
@@ -0,0 +1,38 @@
+import { z } from 'zod';
+import { sendEmail } from '~/emails/send-email.job.ts';
+import { testJob } from '../shared/jobs/test.job.ts';
+import { needsAdminRole } from './authorization.ts';
+
+export const TestEmailSchema = z.object({ to: z.string().email() });
+
+type TestEmail = z.infer<typeof TestEmailSchema>;
+
+export class AdminDebug {
+  private constructor() {}
+
+  static async for(userId: string) {
+    await needsAdminRole(userId);
+    return new AdminDebug();
+  }
+
+  simulateServerError() {
+    throw new Error('Simulated server error');
+  }
+
+  async sendTestJobcall() {
+    await testJob.trigger();
+  }
+
+  async sendTestEmail(email: TestEmail) {
+    return sendEmail.trigger({
+      template: 'base-event-email',
+      subject: 'Test email from Conference Hall',
+      from: 'Test email <no-reply@conference-hall.io>',
+      to: [email.to],
+      data: {
+        children: 'This is a test email from Conference Hall',
+        logoUrl: 'https://conference-hall.io/storage/b6cf57b6-b752-4b8d-a280-02e17b0c5262.jpg',
+      },
+    });
+  }
+}
--- app/routes/admin+/debug.tsx
+++ app/routes/admin+/debug.tsx
@@ -1,45 +1,72 @@
+import { parseWithZod } from '@conform-to/zod';
 import type { ActionFunctionArgs } from '@remix-run/node';
-import { Form } from '@remix-run/react';
-import { testJob } from '~/.server/shared/jobs/test.job';
+import { Form, useActionData } from '@remix-run/react';
+import { AdminDebug, TestEmailSchema } from '~/.server/admin/admin-debug.ts';
 
 import { Button } from '~/design-system/buttons.tsx';
+import { Input } from '~/design-system/forms/input.tsx';
 import { Card } from '~/design-system/layouts/card.tsx';
 import { Page } from '~/design-system/layouts/page.tsx';
-import { H1 } from '~/design-system/typography.tsx';
-
-export const loader = async () => {
-  return null;
-};
+import { H1, H2 } from '~/design-system/typography.tsx';
+import { requireSession } from '~/libs/auth/session.ts';
+import { toast } from '~/libs/toasts/toast.server.ts';
 
 export const action = async ({ request }: ActionFunctionArgs) => {
+  const userId = await requireSession(request);
   const form = await request.formData();
-  const intent = form.get('intent') as string;
+  const debug = await AdminDebug.for(userId);
 
+  const intent = form.get('intent') as string;
   switch (intent) {
     case 'simulate-server-error': {
-      throw new Error('Failed');
+      debug.simulateServerError();
+      break;
     }
     case 'test-job-call': {
-      await testJob.trigger();
+      await debug.sendTestJobcall();
       break;
+    }
+    case 'send-email': {
+      const result = parseWithZod(form, { schema: TestEmailSchema });
+      if (result.status !== 'success') return result.error;
+      await debug.sendTestEmail(result.value);
+      return toast('success', 'Email sent');
     }
   }
   return null;
 };
 
 export default function AdminDebugPage() {
+  const errors = useActionData<typeof action>();
+
   return (
-    <Page>
-      <H1 srOnly>Users</H1>
-      <Card className="p-8">
-        <Form method="POST" className="space-x-8">
-          <Button type="submit" name="intent" value="test-job-call" variant="secondary">
-            Test job call
-          </Button>
-          <Button type="submit" name="intent" value="simulate-server-error" variant="secondary">
-            Simulate server error
-          </Button>
-        </Form>
+    <Page className="space-y-6">
+      <H1 srOnly>Debug page</H1>
+
+      <Card>
+        <Card.Content>
+          <H2>Debug tools</H2>
+          <Form method="POST" className="space-x-8">
+            <Button type="submit" name="intent" value="test-job-call" variant="secondary">
+              Test job call
+            </Button>
+            <Button type="submit" name="intent" value="simulate-server-error" variant="secondary">
+              Simulate server error
+            </Button>
+          </Form>
+        </Card.Content>
+      </Card>
+
+      <Card>
+        <Card.Content>
+          <H2>Email tester</H2>
+          <Form method="POST" className="flex gap-2">
+            <Input type="email" name="to" placeholder="Email" error={errors?.to} required />
+            <Button type="submit" name="intent" value="send-email" variant="secondary">
+              Send email
+            </Button>
+          </Form>
+        </Card.Content>
       </Card>
     </Page>
   );
