70e26e72b2b3f50b1ed6cebf9a2e32a72f3773c0
Benjamin Petetot <bpetetot@gmail.com>
2024-11-15 19:15:35 +0100

    fix: ensure logo in email is a https image

--- app/emails/templates/base-event-email.tsx
+++ app/emails/templates/base-event-email.tsx
@@ -1,14 +1,14 @@
 import { Img } from '@react-email/components';
 import BaseEmail, { styles, type BaseEmailProps } from './base-email.tsx';
 
-type BaseEventEmailProps = BaseEmailProps & {
-  logoUrl: string | null;
-};
+type BaseEventEmailProps = BaseEmailProps & { logoUrl: string | null };
 
 export default function BaseEventEmail({ logoUrl, children, lang }: BaseEventEmailProps) {
   return (
     <BaseEmail lang={lang}>
-      {logoUrl ? <Img className={styles.logo} height={100} src={logoUrl} /> : null}
+      {logoUrl?.startsWith('https://') ? (
+        <Img className={styles.logo} height={70} width={70} src={logoUrl} alt="" />
+      ) : null}
 
       {children}
     </BaseEmail>
