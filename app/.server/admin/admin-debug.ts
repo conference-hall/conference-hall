1aa5c1e9c7d04312ac43655b33a684d2d7849b1e
Benjamin Petetot <bpetetot@gmail.com>
2024-11-16 14:11:39 +0100

    refactor: add content in test email for debug

--- app/.server/admin/admin-debug.ts
+++ app/.server/admin/admin-debug.ts
@@ -26,11 +26,12 @@ export class AdminDebug {
   async sendTestEmail(email: TestEmail) {
     return sendEmail.trigger({
       template: 'base-event-email',
-      subject: 'Test email from Conference Hall',
-      from: 'Test email <no-reply@mg.conference-hall.io>',
+      subject: 'Conference Hall',
+      from: 'no-reply@mg.conference-hall.io',
       to: [email.to],
       data: {
-        children: 'This is a test email from Conference Hall',
+        children:
+          'Dear Ben, We’re excited to welcome you as a speaker at our upcoming conference! Your session will be held in Conference Hall A, equipped with a full AV setup, including a projector, microphones, and a sound system. The hall will be open for you to test your presentation 30 minutes before your scheduled time. If you have any specific requirements or need assistance, please let us know in advance. We’re here to ensure everything runs smoothly so you can focus on delivering an outstanding talk. Looking forward to your presentation!',
         logoUrl: 'https://conference-hall.io/storage/b6cf57b6-b752-4b8d-a280-02e17b0c5262.jpg',
       },
     });
