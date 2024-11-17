54d7f71ce3c5c3c474f07be5ea48484faad5f554
Benjamin Petetot <bpetetot@gmail.com>
2024-11-17 10:50:37 +0100

    feat: add survey-custom feature flag

--- flags.config.ts
+++ flags.config.ts
@@ -1,6 +1,11 @@
 import { defineFlagsConfig } from './app/libs/feature-flags/flags-client';
 
 export default defineFlagsConfig({
+  'custom-survey': {
+    description: 'Enables custom survey feature.',
+    type: 'boolean',
+    defaultValue: false,
+  },
   seo: {
     description: 'Enables SEO features like sitemap.xml and robots.txt.',
     type: 'boolean',
