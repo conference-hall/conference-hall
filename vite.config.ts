cbfa3c85e243ef5100fc2114cfb258d165a705fc
Benjamin Petetot <516360+bpetetot@users.noreply.github.com>
2024-11-24 12:57:57 +0100

    refactor: enable Remix v3 route config (#356)

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import mdx from '@mdx-js/rollup';
 import { vitePlugin as remix } from '@remix-run/dev';
 import { sentryVitePlugin } from '@sentry/vite-plugin';
-import { flatRoutes } from 'remix-flat-routes';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -25,19 +24,13 @@ export default defineConfig({
     expressDevServer(),
     mdx(),
     remix({
-      serverModuleFormat: 'esm',
-      ignoredRouteFiles: ['**/*'],
-      routes: async (defineRoutes) => {
-        return flatRoutes('routes', defineRoutes, {
-          ignoredRouteFiles: ['.*', '**/__components/*', '**/*.css', '**/*.test.{js,jsx,ts,tsx}'],
-        });
-      },
       future: {
         v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
         v3_lazyRouteDiscovery: true,
+        v3_relativeSplatPath: true,
+        v3_routeConfig: true,
         v3_singleFetch: true,
+        v3_throwAbortReason: true,
         unstable_optimizeDeps: true,
       },
     }),
