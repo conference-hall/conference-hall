9ce3353462f467ca5d09c0a722374646be2810ad
Benjamin Petetot <bpetetot@gmail.com>
2024-12-08 18:35:05 +0100

    refactor: use zod to check existing slugs

--- app/libs/validators/slug.ts
+++ app/libs/validators/slug.ts
@@ -5,4 +5,31 @@ export const SlugSchema = z
   .regex(/^[a-z0-9\\-]*$/, { message: 'Must only contain lower case alphanumeric and dashes (-).' })
   .trim()
   .min(3)
-  .max(50);
+  .max(50)
+  .refine((slug) => !RESERVED_SLUGS.includes(slug), { message: 'This URL is reserved.' });
+
+const RESERVED_SLUGS = [
+  'new',
+  'admin',
+  'request',
+  'api',
+  'auth',
+  'team',
+  'teams',
+  'user',
+  'users',
+  'event',
+  'events',
+  'settings',
+  'speaker',
+  'speakers',
+  'profile',
+  'account',
+  'public',
+  'assets',
+  'invite',
+  'docs',
+  'notifications',
+  'storage',
+  'healthcheck',
+];
