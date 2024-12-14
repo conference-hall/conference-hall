b303f76fb021931853ee0e5891225eb6034abc54
Benjamin Petetot <516360+bpetetot@users.noreply.github.com>
2024-12-14 22:24:21 +0100

    refactor: web server and build (#374)

--- app/types/_react-router.module.ts
+++
@@ -1,8 +0,0 @@
-// Declare module for react-router context
-declare module 'react-router' {
-  interface AppLoadContext {
-    cspNonce: string;
-  }
-}
-
-export {}; // necessary for TS to treat this as a module
---
+++ servers/web.prod.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import { setupExpressServer } from './express/setup-express-server.ts';
+
+const BUILD_PATH = './app.js';
+
+await setupExpressServer(async (app) => {
+  console.log('🚀 Starting production server');
+
+  app.use('/fonts', express.static('build/client/fonts', { immutable: true, maxAge: '1y' }));
+  app.use('/assets', express.static('build/client/assets', { immutable: true, maxAge: '1y' }));
+  app.use(express.static('build/client', { maxAge: '1h' }));
+
+  app.use(await import(BUILD_PATH).then((mod) => mod.app));
+});
