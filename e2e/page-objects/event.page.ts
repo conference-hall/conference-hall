c01259a7d4a7a8f925cafa860bdd31f7f4092b43
Benjamin Petetot <bpetetot@gmail.com>
2024-12-15 20:07:51 +0100

    feat: add homepage tests

---
+++ e2e/page-objects/event.page.ts
@@ -0,0 +1,18 @@
+import type { Page } from '@playwright/test';
+
+export class EventPage {
+  readonly page: Page;
+
+  constructor(page: Page) {
+    this.page = page;
+  }
+
+  async goto(slug: string, name: string) {
+    await this.page.goto(`/${slug}`);
+    await this.heading(name).waitFor();
+  }
+
+  heading(name: string) {
+    return this.page.getByRole('heading', { name });
+  }
+}
---
+++ e2e/page-objects/home.page.ts
@@ -0,0 +1,37 @@
+import type { Locator, Page } from '@playwright/test';
+import { EventPage } from './event.page.ts';
+
+export class HomePage {
+  readonly page: Page;
+  readonly heading: Locator;
+  readonly searchInput: Locator;
+  readonly results: Locator;
+  readonly noResults: Locator;
+
+  constructor(page: Page) {
+    this.page = page;
+    this.heading = this.page.getByRole('heading', { name: 'Call for papers for conferences and meetups.' });
+    this.searchInput = this.page.getByLabel('Search conferences and meetups.');
+    this.results = this.page.getByRole('list', { name: 'Search results' }).locator('li');
+    this.noResults = this.page.getByText('No results found!');
+  }
+
+  async goto() {
+    await this.page.goto('/');
+    await this.heading.waitFor();
+  }
+
+  async search(query: string) {
+    await this.searchInput.fill(query);
+    await this.searchInput.press('Enter');
+  }
+
+  item(name: RegExp) {
+    return this.page.getByRole('link', { name });
+  }
+
+  async openEventPage(name: RegExp) {
+    await this.item(name).click();
+    return new EventPage(this.page);
+  }
+}
