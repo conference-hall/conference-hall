378b8e1a3160999c9b89b9d3e96972f8f6924d3d
Benjamin Petetot <bpetetot@gmail.com>
2024-12-15 17:13:05 +0100

    feat: setup playwright

---
+++ e2e/setup/fixtures.ts
@@ -0,0 +1,27 @@
+import { test as base } from '@playwright/test';
+import { disconnectDB, resetDB } from 'tests/db-helpers.ts';
+import { type TestUser, getUserAuthPath } from './helpers.ts';
+
+// biome-ignore lint: test file
+export const test = base.extend<{}, { forEachWorker: void }>({
+  forEachWorker: [
+    // biome-ignore lint: test file
+    async ({}, use) => {
+      // This code runs before all the tests in the worker process.
+      await resetDB();
+
+      // Execute the test.
+      await use();
+
+      // This code runs after all the tests in the worker process.
+      await disconnectDB();
+    },
+    { scope: 'worker', auto: true },
+  ],
+});
+
+export const loginWith = (user: TestUser) => {
+  test.use({ storageState: getUserAuthPath(user) });
+};
+
+export { expect } from '@playwright/test';
