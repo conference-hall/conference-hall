0221953fb893c78988fa0a94a224cb31cc862e33
Benjamin Petetot <bpetetot@gmail.com>
2024-12-21 17:21:30 +0100

    feat: add event-page/survey tests

---
+++ e2e/tests/event-page/survey.page.ts
@@ -0,0 +1,24 @@
+import type { Locator, Page } from '@playwright/test';
+import { PageObject } from 'e2e/page-object.ts';
+
+export class SurveyPage extends PageObject {
+  readonly heading: Locator;
+
+  constructor(page: Page) {
+    super(page);
+    this.heading = page.getByRole('heading', { name: 'We have some questions for you.' });
+  }
+
+  async goto(slug: string) {
+    await this.page.goto(`/${slug}/survey`);
+    await this.waitFor();
+  }
+
+  async waitFor() {
+    await this.heading.waitFor();
+  }
+
+  async clickOnSave() {
+    await this.page.getByRole('button', { name: 'Save survey' }).click();
+  }
+}
