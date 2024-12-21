9052bcb1814573da1664a7c8e2114a9cd32ca35a
Benjamin Petetot <bpetetot@gmail.com>
2024-12-21 17:39:32 +0100

    feat: add event-page/proposal-list tests

--- e2e/tests/event-page/proposal-list.page.ts
+++ e2e/tests/event-page/proposal-list.page.ts
@@ -1,14 +1,17 @@
 import type { Locator, Page } from '@playwright/test';
 import { PageObject } from 'e2e/page-object.ts';
+import { ProposalPage } from './proposal.page.ts';
 
 export class ProposalListPage extends PageObject {
   readonly heading: Locator;
   readonly proposals: Locator;
+  readonly noProposals: Locator;
 
   constructor(page: Page) {
     super(page);
     this.heading = page.getByRole('heading', { name: 'Your proposals' });
     this.proposals = page.getByRole('list', { name: 'Proposals list' }).locator('>li');
+    this.noProposals = page.getByText('No proposals submitted!');
   }
 
   async goto(slug: string) {
@@ -22,5 +25,10 @@ export class ProposalListPage extends PageObject {
 
   proposal(title: string) {
     return this.proposals.getByRole('link', { name: title });
+  }
+
+  async clickOnProposal(title: string) {
+    await this.proposal(title).click();
+    return new ProposalPage(this.page);
   }
 }
