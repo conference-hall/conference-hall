1108ab02a1eefbf78c6be58a087766997fd019cb
Benjamin Petetot <bpetetot@gmail.com>
2024-12-22 19:48:46 +0100

    feat: add team/team-settings members tests

--- e2e/tests/team/team-settings.page.ts
+++ e2e/tests/team/team-settings.page.ts
@@ -8,6 +8,8 @@ export class TeamSettingsPage extends PageObject {
   readonly membersPage: Locator;
   readonly nameInput: Locator;
   readonly slugInput: Locator;
+  readonly members: Locator;
+  readonly findMember: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -16,6 +18,8 @@ export class TeamSettingsPage extends PageObject {
     this.membersPage = page.getByRole('heading', { name: 'Members' });
     this.nameInput = page.getByRole('textbox', { name: 'Team name' });
     this.slugInput = page.getByRole('textbox', { name: 'Team URL' });
+    this.members = page.getByRole('list', { name: 'Members list' }).locator('>li');
+    this.findMember = page.getByLabel('Find member');
   }
 
   async goto(slug: string) {
@@ -26,6 +30,8 @@ export class TeamSettingsPage extends PageObject {
   async waitFor() {
     await this.heading.waitFor();
   }
+
+  // General settings
 
   async fillForm(name: string, slug: string) {
     await this.nameInput.fill(name);
@@ -43,5 +49,41 @@ export class TeamSettingsPage extends PageObject {
   async clickOnLeaveTeam(teamName: string) {
     await this.leaveTeamButton(teamName).click();
     return new ActivityPage(this.page);
+  }
+
+  // Member list
+
+  async gotoMembers(slug: string) {
+    await this.page.goto(`/team/${slug}/settings/members`);
+    await this.waitFor();
+  }
+
+  member(name: string) {
+    return this.members.getByText(name);
+  }
+
+  async clickOnInviteMember() {
+    await this.page.getByRole('button', { name: 'Invite member' }).click();
+    return this.page.getByLabel('Copy invitation link');
+  }
+
+  removeMemberButton(name: string) {
+    return this.members.getByRole('button', { name: `Remove ${name} from team` });
+  }
+
+  changeRoleButton(name: string) {
+    return this.members.getByLabel(`Change role of ${name}`);
+  }
+
+  async clickOnRole(role: string) {
+    await this.page.getByRole('radio', { name: role }).click();
+  }
+
+  async clickOnConfirmRole(name: string) {
+    await this.page.getByRole('button', { name: `Change ${name}'s role` }).click();
+  }
+
+  async clickOnConfirmRemove(name: string) {
+    await this.page.getByRole('button', { name: `Remove ${name}`, exact: true }).click();
   }
 }
