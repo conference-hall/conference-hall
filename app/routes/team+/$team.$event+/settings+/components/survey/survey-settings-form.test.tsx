5608ab4bfaf564806c6d4fa6c01fbe883392a0f3
Benjamin Petetot <bpetetot@gmail.com>
2025-02-02 19:05:04 +0100

    refactor: update test

--- app/routes/team+/$team.$event+/settings+/components/survey/survey-settings-form.test.tsx
+++ app/routes/team+/$team.$event+/settings+/components/survey/survey-settings-form.test.tsx
@@ -1,26 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
+import { userEvent } from '@vitest/browser/context';
 import { createRoutesStub } from 'react-router';
+import { render } from 'vitest-browser-react';
 import { SurveySettingsForm, type SurveySettingsFormProps } from './survey-settings-form.tsx';
 
 describe('SurveySettingsForm component', () => {
   const renderComponent = (config: SurveySettingsFormProps) => {
     const RouteStub = createRoutesStub([{ path: '/', Component: () => <SurveySettingsForm {...config} /> }]);
-    return render(<RouteStub />);
+    return render(<RouteStub initialEntries={['/']} />);
   };
 
-  it('renders the survey settings form', () => {
+  it('renders the survey settings form', async () => {
     const config = {
       legacy: false,
       enabled: true,
       questions: [],
     };
 
-    renderComponent({ config });
+    const screen = renderComponent({ config });
 
-    expect(screen.getByText('Speaker survey')).toBeInTheDocument();
-    expect(screen.getByText('New')).toBeInTheDocument();
-    expect(screen.getByText('Speaker survey activation')).toBeInTheDocument();
+    await expect.element(screen.getByRole('heading', { name: 'Speaker survey' })).toBeInTheDocument();
+    await expect.element(screen.getByText(/New/)).toBeInTheDocument();
+    await expect.element(screen.getByText(/Speaker survey activation/)).toBeInTheDocument();
   });
 
   it('adds a new question', async () => {
@@ -30,10 +30,10 @@ describe('SurveySettingsForm component', () => {
       questions: [],
     };
 
-    renderComponent({ config });
+    const screen = renderComponent({ config });
 
     await userEvent.click(screen.getByRole('button', { name: 'Add question' }));
 
-    expect(screen.getByRole('dialog')).toBeInTheDocument();
+    await expect.element(screen.getByRole('dialog')).toBeInTheDocument();
   });
 });
