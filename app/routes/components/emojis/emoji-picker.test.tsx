4c48a0d708e6d8404eaa6439adf22bef99991331
Benjamin Petetot <bpetetot@gmail.com>
2025-02-02 18:53:46 +0100

    refactor: update test

--- app/routes/components/emojis/emoji-picker.test.tsx
+++ app/routes/components/emojis/emoji-picker.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import { userEvent } from '@testing-library/user-event';
+import { userEvent } from '@vitest/browser/context';
+import { render } from 'vitest-browser-react';
 import type { Emoji } from '../../../types/emojis.types.ts';
 import { EmojiPicker } from './emoji-picker.tsx';
 
@@ -12,32 +12,33 @@ describe('EmojiPicker component', () => {
   it('displays emoji picker and select an emoji', async () => {
     const onSelectEmoji = vi.fn();
 
-    render(<EmojiPicker emojis={EMOJIS} onSelectEmoji={onSelectEmoji} />);
+    const screen = render(<EmojiPicker emojis={EMOJIS} onSelectEmoji={onSelectEmoji} />);
 
     const button = screen.getByRole('button', { name: 'Select a reaction' });
-    expect(button).toHaveAttribute('aria-expanded', 'false');
-    await userEvent.click(button);
-    expect(button).toHaveAttribute('aria-expanded', 'true');
+    await expect.element(button).toHaveAttribute('aria-expanded', 'false');
 
-    const thumbsUpButton = await screen.findByRole('button', { name: 'Thumbs up' });
+    await userEvent.click(button);
+    await expect.element(button).toHaveAttribute('aria-expanded', 'true');
+
+    const thumbsUpButton = screen.getByRole('button', { name: 'Thumbs up' });
     await userEvent.click(thumbsUpButton);
 
-    expect(button).toHaveAttribute('aria-expanded', 'false');
+    await expect.element(button).toHaveAttribute('aria-expanded', 'false');
     expect(onSelectEmoji).toHaveBeenCalledWith(EMOJIS.at(0));
   });
 
   it('can disable some emojis', async () => {
     const onSelectEmoji = vi.fn();
 
-    render(<EmojiPicker emojis={EMOJIS} disabledEmojis={['+1']} onSelectEmoji={onSelectEmoji} />);
+    const screen = render(<EmojiPicker emojis={EMOJIS} disabledEmojis={['+1']} onSelectEmoji={onSelectEmoji} />);
 
     const button = screen.getByRole('button', { name: 'Select a reaction' });
     await userEvent.click(button);
 
-    const thumbsUpButton = await screen.findByRole('button', { name: 'Thumbs up' });
-    expect(thumbsUpButton).toHaveAttribute('disabled');
+    const thumbsUpButton = await screen.getByRole('button', { name: 'Thumbs up' });
+    await expect.element(thumbsUpButton).toHaveAttribute('disabled');
 
-    const thumbsDownButton = await screen.findByRole('button', { name: 'Thumbs down' });
-    expect(thumbsDownButton).not.toHaveAttribute('disabled');
+    const thumbsDownButton = await screen.getByRole('button', { name: 'Thumbs down' });
+    await expect.element(thumbsDownButton).not.toHaveAttribute('disabled');
   });
 });
