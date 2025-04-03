f11089fa880c24703134a19e3ad3aa6405b27f87
Benjamin Petetot <bpetetot@gmail.com>
2025-04-03 14:24:54 +0200

    fix: i18n init in entry.client.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,26 +4,15 @@ import { I18nextProvider } from 'react-i18next';
 import { HydratedRouter } from 'react-router/dom';
 import { initializeI18n } from './libs/i18n/i18n.browser.ts';
 
-async function hydrate() {
-  const i18n = await initializeI18n();
+const i18n = await initializeI18n();
 
-  startTransition(() => {
-    hydrateRoot(
-      document,
-      <I18nextProvider i18n={i18n}>
-        <StrictMode>
-          <HydratedRouter />
-        </StrictMode>
-      </I18nextProvider>,
-    );
-  });
-}
-
-// todo(i18n): Really need to use requestIdleCallback here?
-if (window.requestIdleCallback) {
-  window.requestIdleCallback(hydrate);
-} else {
-  // Safari doesn't support requestIdleCallback
-  // https://caniuse.com/requestidlecallback
-  window.setTimeout(hydrate, 1);
-}
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <I18nextProvider i18n={i18n}>
+      <StrictMode>
+        <HydratedRouter />
+      </StrictMode>
+    </I18nextProvider>,
+  );
+});
