b8ad0dc93380b304f2f33f61d459bf47e1e717dc
Benjamin Petetot <bpetetot@gmail.com>
2025-04-06 13:25:06 +0200

    refactor: extract keys from event/proposals/proposal route

--- app/routes/components/talks/talk-forms/talk-submit-button.tsx
+++ app/routes/components/talks/talk-forms/talk-submit-button.tsx
@@ -1,13 +1,14 @@
 import { PaperAirplaneIcon } from '@heroicons/react/24/outline';
-
+import { useTranslation } from 'react-i18next';
 import { ButtonLink } from '~/design-system/buttons.tsx';
 
 type Props = { talkId: string };
 
 export function TalkSubmitButton({ talkId }: Props) {
+  const { t } = useTranslation();
   return (
     <ButtonLink to={{ pathname: '/', search: `?talkId=${talkId}` }} iconLeft={PaperAirplaneIcon}>
-      Submit
+      {t('common.submit')}
     </ButtonLink>
   );
 }
