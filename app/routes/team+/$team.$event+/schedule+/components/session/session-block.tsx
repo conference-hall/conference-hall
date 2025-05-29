c7f7bd86d770b8b25db9a7d35cdf16a16aeaec52
Benjamin Petetot <bpetetot@gmail.com>
2025-05-29 15:43:12 +0200

    fix

--- app/routes/team+/$team.$event+/schedule+/components/session/session-block.tsx
+++ app/routes/team+/$team.$event+/schedule+/components/session/session-block.tsx
@@ -38,31 +38,33 @@ export function SessionBlock({
   if (size === 'xs') return <div className={cx('h-full', block)} />;
 
   return (
-    <button
-      type="button"
-      onClick={() => setEdit(true)}
-      className={cx(
-        'flex flex-col h-full w-full text-left px-1 rounded-sm cursor-pointer',
-        {
-          'text-[10px] items-center gap-1 flex-row': size === 'sm',
-          'text-[10px] items-baseline leading-3 gap-1 flex-row': size === 'md',
-          'text-xs leading-3.5 justify-between': size === 'lg' || size === 'xl',
-        },
-        block,
-      )}
-    >
-      {title ? (
-        <div className={cx({ truncate: size !== 'xl' })}>
-          <p className={cx('font-semibold line-clamp-3', { truncate: size !== 'xl' })}>{title}</p>
-          <SessionSpeakers speakers={proposal?.speakers} size={size} />
-        </div>
-      ) : null}
+    <>
+      <button
+        type="button"
+        onClick={() => setEdit(true)}
+        className={cx(
+          'flex flex-col h-full w-full text-left px-1 rounded-sm cursor-pointer',
+          {
+            'text-[10px] items-center gap-1 flex-row': size === 'sm',
+            'text-[10px] items-baseline leading-3 gap-1 flex-row': size === 'md',
+            'text-xs leading-3.5 justify-between': size === 'lg' || size === 'xl',
+          },
+          block,
+        )}
+      >
+        {title ? (
+          <div className={cx({ truncate: size !== 'xl' })}>
+            <p className={cx('font-semibold line-clamp-3', { truncate: size !== 'xl' })}>{title}</p>
+            <SessionSpeakers speakers={proposal?.speakers} size={size} />
+          </div>
+        ) : null}
 
-      <div className={cx('flex shrink-0 gap-1', { 'mt-0.5': size === 'md', 'items-end': !title })}>
-        <SessionTime timeslot={timeslot} size={size} />
-        <SessionEmojis emojis={emojis} size={size} />
-        <SessionLanguage language={language} size={size} />
-      </div>
+        <div className={cx('flex shrink-0 gap-1', { 'mt-0.5': size === 'md', 'items-end': !title })}>
+          <SessionTime timeslot={timeslot} size={size} />
+          <SessionEmojis emojis={emojis} size={size} />
+          <SessionLanguage language={language} size={size} />
+        </div>
+      </button>
 
       {edit && (
         <SessionModal
@@ -74,7 +76,7 @@ export function SessionBlock({
           onClose={() => setEdit(false)}
         />
       )}
-    </button>
+    </>
   );
 }
 
