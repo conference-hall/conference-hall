e925b37b6cc8222ccb59fa665948eb0965477966
Benjamin Petetot <bpetetot@gmail.com>
2025-06-28 00:17:08 +0200

    fix: lint

--- app/routes/team.event-management/proposals/exports/cards.tsx
+++ app/routes/team.event-management/proposals/exports/cards.tsx
@@ -19,46 +19,44 @@ export const loader = async ({ request, params }: Route.LoaderArgs) => {
 export default function ExportProposalsCards({ loaderData: results }: Route.ComponentProps) {
   const { t } = useTranslation();
   return (
-    <>
-      <div className="layout">
-        {results?.map((proposal) => (
-          <div key={proposal.id} className="card">
-            <div className="grow">
-              <Text size="l" weight="medium" truncate>
-                {proposal.title}
-              </Text>
-              <Subtitle truncate>{proposal.speakers?.join(', ')}</Subtitle>
+    <div className="layout">
+      {results?.map((proposal) => (
+        <div key={proposal.id} className="card">
+          <div className="grow">
+            <Text size="l" weight="medium" truncate>
+              {proposal.title}
+            </Text>
+            <Subtitle truncate>{proposal.speakers?.join(', ')}</Subtitle>
+          </div>
+          <div>
+            <div className="grid grid-cols-2 grid-rows-2 gap-2">
+              <div className="rounded-sm border border-gray-400 p-2">
+                <Subtitle truncate>{proposal.formats.map((f) => f.name).join(', ') || '-'}</Subtitle>
+              </div>
+              <div className="rounded-sm border border-gray-400 p-2">
+                <Subtitle truncate>{proposal.categories.map((c) => c.name).join(', ') || '-'}</Subtitle>
+              </div>
+              <div className="rounded-sm border border-gray-400 p-2">
+                <Subtitle truncate>{proposal.level ? t(`common.level.${proposal.level}`) : '-'}</Subtitle>
+              </div>
+              <div className="rounded-sm border border-gray-400 p-2">
+                <Subtitle truncate>
+                  {proposal.languages.map((lang) => t(`common.languages.${lang}.label`)).join(', ') || '-'}
+                </Subtitle>
+              </div>
             </div>
-            <div>
-              <div className="grid grid-cols-2 grid-rows-2 gap-2">
-                <div className="rounded-sm border border-gray-400 p-2">
-                  <Subtitle truncate>{proposal.formats.map((f) => f.name).join(', ') || '-'}</Subtitle>
-                </div>
-                <div className="rounded-sm border border-gray-400 p-2">
-                  <Subtitle truncate>{proposal.categories.map((c) => c.name).join(', ') || '-'}</Subtitle>
-                </div>
-                <div className="rounded-sm border border-gray-400 p-2">
-                  <Subtitle truncate>{proposal.level ? t(`common.level.${proposal.level}`) : '-'}</Subtitle>
-                </div>
-                <div className="rounded-sm border border-gray-400 p-2">
-                  <Subtitle truncate>
-                    {proposal.languages.map((lang) => t(`common.languages.${lang}.label`)).join(', ') || '-'}
-                  </Subtitle>
-                </div>
+            <div className="flex items-center justify-between pt-4">
+              <div className="flex items-center gap-4">
+                <Text size="base">❤️ {proposal.reviews?.positives}</Text>
+                <Text size="base">💀 {proposal.reviews?.negatives}</Text>
               </div>
-              <div className="flex items-center justify-between pt-4">
-                <div className="flex items-center gap-4">
-                  <Text size="base">❤️ {proposal.reviews?.positives}</Text>
-                  <Text size="base">💀 {proposal.reviews?.negatives}</Text>
-                </div>
-                <Text size="3xl" weight="medium">
-                  {formatReviewNote(proposal.reviews?.average)}
-                </Text>
-              </div>
+              <Text size="3xl" weight="medium">
+                {formatReviewNote(proposal.reviews?.average)}
+              </Text>
             </div>
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+    </div>
   );
 }
