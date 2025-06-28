b1d1170aca74c20c36d82068f19f3df139a6088b
Benjamin Petetot <bpetetot@gmail.com>
2025-06-28 23:07:18 +0200

    fixup! fixup! feat: setup db schema for email customization

--- prisma/migrations/20250628203747_add_email_customization/migration.sql
+++ prisma/migrations/20250628203747_add_email_customization/migration.sql
@@ -1,15 +1,11 @@
--- CreateEnum
-CREATE TYPE "EventEmailType" AS ENUM ('PROPOSAL_SUBMITTED', 'PROPOSAL_ACCEPTED', 'PROPOSAL_DECLINED');
-
 -- CreateTable
 CREATE TABLE "event_email_customizations" (
     "id" TEXT NOT NULL,
     "eventId" TEXT NOT NULL,
-    "emailType" "EventEmailType" NOT NULL,
-    "locale" TEXT NOT NULL DEFAULT 'en',
+    "template" TEXT NOT NULL,
+    "locale" TEXT NOT NULL,
     "subject" TEXT,
     "content" TEXT,
-    "signature" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
@@ -17,7 +13,7 @@ CREATE TABLE "event_email_customizations" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "event_email_customizations_eventId_emailType_locale_key" ON "event_email_customizations"("eventId", "emailType", "locale");
+CREATE UNIQUE INDEX "event_email_customizations_eventId_template_locale_key" ON "event_email_customizations"("eventId", "template", "locale");
 
 -- AddForeignKey
 ALTER TABLE "event_email_customizations" ADD CONSTRAINT "event_email_customizations_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "events"("id") ON DELETE CASCADE ON UPDATE CASCADE;
