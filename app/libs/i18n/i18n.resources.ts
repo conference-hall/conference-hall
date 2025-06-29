da62b4bb0ff74f1dec40eaadf703b6b1688825d1
Benjamin Petetot <bpetetot@gmail.com>
2025-06-29 16:46:32 +0200

    setup email namespace for i18n

--- app/libs/i18n/i18n.resources.ts
+++ app/libs/i18n/i18n.resources.ts
@@ -1,7 +1,10 @@
-import en from '../../locales/en.json' with { type: 'json' };
-import fr from '../../locales/fr.json' with { type: 'json' };
+import en_email from '../../locales/en.email.json' with { type: 'json' };
+import en_translation from '../../locales/en.translation.json' with { type: 'json' };
+
+import fr_email from '../../locales/fr.email.json' with { type: 'json' };
+import fr_translation from '../../locales/fr.translation.json' with { type: 'json' };
 
 export const i18nResources = {
-  en: { translation: en },
-  fr: { translation: fr },
+  en: { translation: en_translation, email: en_email },
+  fr: { translation: fr_translation, email: fr_email },
 };
