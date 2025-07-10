198c2206ce4905ad1f0dadb643945b27da99ddbf
Benjamin Petetot <bpetetot@gmail.com>
2025-07-10 12:18:21 +0200

    refactor: remove feature flags table

---
+++ prisma/migrations/20250710101803_remove_feature_flags_table/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the `feature_flags` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "feature_flags";
