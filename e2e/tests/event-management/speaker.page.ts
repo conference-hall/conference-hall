0d235441413b186b60c6cc962c231a1596f56c5f
Benjamin Petetot <bpetetot@gmail.com>
2025-10-03 12:23:52 +0200

    feat: add e2e tests

--- e2e/tests/event-management/speaker.page.ts
+++ e2e/tests/event-management/speaker.page.ts
@@ -4,6 +4,7 @@ export class SpeakerPage extends PageObject {
   readonly referencesSection = this.page.getByText('References');
   readonly proposalsHeader = this.page.getByText(/Proposals \(\d+\)/);
   readonly emptyState = this.page.getByText('No proposals.');
+  readonly newProposalButton = this.page.getByRole('link', { name: 'Proposal', exact: true });
 
   async goto(team: string, event: string, speaker: string) {
     await this.page.goto(`/team/${team}/${event}/speakers/${speaker}`);
@@ -16,5 +17,9 @@ export class SpeakerPage extends PageObject {
 
   async clickProposal(title: string) {
     await this.page.getByRole('link', { name: new RegExp(title) }).click();
+  }
+
+  async clickNewProposal() {
+    await this.newProposalButton.click();
   }
 }
