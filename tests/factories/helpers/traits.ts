82bfd9641b9e094e5d4b45d1da1cd3376bdd8be3
Benjamin Petetot <bpetetot@gmail.com>
2025-11-21 19:56:49 +0100

    chore: upgrade dependencies

--- tests/factories/helpers/traits.ts
+++ tests/factories/helpers/traits.ts
@@ -2,7 +2,6 @@ export function applyTraits<T extends Record<string, unknown>, S extends string[
   return traits?.reduce((attributes, trait) => {
     const selected = TRAITS?.[trait];
     if (selected) {
-      // biome-ignore lint/performance/noAccumulatingSpread: test helper
       return Object.assign(attributes, selected);
     }
     return attributes;
