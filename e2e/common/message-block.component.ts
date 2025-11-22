75f8b56cdfd5c48cab916acf766df133361ac999
Benjamin Petetot <bpetetot@gmail.com>
2025-11-22 13:04:13 +0100

    fix: flaky on proposals message menu

--- e2e/common/message-block.component.ts
+++ e2e/common/message-block.component.ts
@@ -12,6 +12,7 @@ export class MessageBlockComponent extends PageObject {
 
   async openActionsMenu() {
     await this.component.getByRole('button', { name: 'Message action menu' }).click();
+    await this.page.getByRole('menu', { name: 'Message action menu' }).waitFor();
   }
 
   async clickEdit() {
