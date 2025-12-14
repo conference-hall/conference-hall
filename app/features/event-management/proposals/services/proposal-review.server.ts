d535e7be5a1288d1ef3e89d78d61362e9eccd6fb
Benjamin Petetot <bpetetot@gmail.com>
2025-12-14 12:44:50 +0100

    feat: add archivedAt when fetching a proposal

--- app/features/event-management/proposals/services/proposal-review.server.ts
+++ app/features/event-management/proposals/services/proposal-review.server.ts
@@ -58,6 +58,7 @@ export class ProposalReview extends EventAuthorization {
       deliberationStatus: proposal.deliberationStatus,
       publicationStatus: proposal.publicationStatus,
       confirmationStatus: proposal.confirmationStatus,
+      archivedAt: proposal.archivedAt?.toISOString() ?? null,
       createdAt: proposal.createdAt,
       languages: proposal.languages as Languages,
       formats: proposal.formats.map(({ id, name }) => ({ id, name })),
