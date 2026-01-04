311122b3e93a56395ce39f05fa5ac2101fcec6b0
Benjamin Petetot <bpetetot@gmail.com>
2026-01-04 14:41:57 +0100

    feat: add proposal id resolver

---
+++ app/features/event-management/proposals/services/proposal-id-resolver.server.ts
@@ -0,0 +1,19 @@
+import { db } from 'prisma/db.server.ts';
+import z from 'zod';
+import type { AuthorizedEvent } from '~/shared/authorization/types.ts';
+import { ProposalNotFoundError } from '~/shared/errors.server.ts';
+
+// todo(number): add tests
+export async function resolveProposalId(authorizedEvent: AuthorizedEvent, proposalParam: string) {
+  const result = z.coerce.number().safeParse(proposalParam);
+  if (!result.success) return proposalParam;
+
+  const proposal = await db.proposal.findUnique({
+    select: { id: true },
+    where: { proposalNumber_eventId: { proposalNumber: result.data, eventId: authorizedEvent.event.id } },
+  });
+
+  if (!proposal) throw new ProposalNotFoundError();
+
+  return proposal.id;
+}
