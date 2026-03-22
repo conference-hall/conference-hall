e33377e0e9cb645a0fa49ce3e90122068dcdafa5
Benjamin Petetot <bpetetot@gmail.com>
2026-03-22 18:06:17 +0100

    refactor: remove logoUrl field from db

---
+++ prisma/migrations/20260322165716_remove_legacy_logo_url_from_event_model/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `logoUrl` on the `events` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "events" DROP COLUMN "logoUrl";
