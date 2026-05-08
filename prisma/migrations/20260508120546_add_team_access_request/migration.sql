e376f99a522f70ffbcbdcc493c85983c05d4ccfe
Benjamin Petetot <bpetetot@gmail.com>
2026-05-08 14:06:49 +0200

    refactor: make a single migration file

--- prisma/migrations/20260508120546_add_team_access_request/migration.sql
+++ prisma/migrations/20260508120546_add_team_access_request/migration.sql
@@ -1,5 +1,17 @@
+/*
+  Warnings:
+
+  - You are about to drop the `organizer_key_access` table. If the table is not empty, all the data it contains will be lost.
+
+*/
 -- CreateEnum
 CREATE TYPE "TeamAccessRequestStatus" AS ENUM ('PENDING', 'ACCEPTED', 'REJECTED');
+
+-- DropForeignKey
+ALTER TABLE "users" DROP CONSTRAINT "users_organizerKey_fkey";
+
+-- DropTable
+DROP TABLE "organizer_key_access";
 
 -- CreateTable
 CREATE TABLE "team_access_requests" (
